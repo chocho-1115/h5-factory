3c261d41fffcef0d70b8485fd1158e6e3f1a6b30
chocho <849890769@qq.com>
2026-05-21 18:29:41 +0800

    Update http.js

--- common/http.js
+++ common/http.js
@@ -5,12 +5,22 @@ let defaultsConfig = {
     baseURL: '',
     async: true,
     method: 'get',
+    timeout: 5000,
     responseType: 'json',
     headers: {
         'content-type': 'application/x-www-form-urlencoded', // 简单请求 application/x-www-form-urlencoded、multipart/form-data 或 text/plain
         // "content-type": "application/json",
         // "If-Modified-Since": "0" //// HTTP的If-Modified-Since头标签与客户端缓存相互配合，可节约网络流量。
     }
+}
+
+const ERROR_INFO = {
+    // 请求事件错误
+    "REQUEST-EVENT-TIMEOUT": "Request Timeout: The request exceeded the timeout limit",
+    "REQUEST-EVENT-ERROR": "Network Error: Failed to establish a connection",
+    "REQUEST-EVENT-ABORT": "Request Aborted: The request was manually cancelled",
+    // 请求前的代码验证错误/或程序错误
+    "INVALID-URL": "Invalid URL: Expected a non-empty string",
 }
 
 // 配置合并
@@ -33,22 +43,59 @@ function dispatchRequest(config){
     config.method = config.method ? config.method.toUpperCase() : 'GET'
 
     const promise = new Promise(function (resolve, reject) {
-        const handler = function () {
+
+        let isSettled = false
+        // 请求发送之前出错了
+        const sendBeforeError = function (code, message = ''){
+            reject({
+                code,
+                message: ERROR_INFO[code] || message,
+                status: client.status,
+                config,
+            })
+        }
+        // 请求发生之后 
+        const onLoadendHandler = function () {
             if (this.readyState !== 4) return
-            let res = {
-                data: this.response, // this.responseText
-                status: this.status,
-                statusText: this.statusText,
-                config: config,
-                request: this
+            if(isSettled) return
+            isSettled = true
+            const response = {
+                data: client.response,
+                status: client.status,
+                statusText: client.statusText,
+                config,
+                request: client,
+                // headers,
             }
-            if (this.status >= 200 && this.status < 300) {
-                resolve(res)
+            if (client.status >= 200 && client.status < 300) {
+                resolve(response)
             } else {
+                const res = {
+                    code: client.status,
+                    message: client.statusText,
+                    status: client.status,
+                    config,
+                    request: client
+                }
+                if (client.response) res.response = response
                 reject(res)
             }
         }
-		
+        // 请求发生之后 错误事件发生时
+        const onErrorHandler = function (e) {    
+            if(isSettled) return
+            isSettled = true
+            const code = `REQUEST-EVENT-${e.type.toUpperCase()}`
+            const res = {
+                code,
+                message: ERROR_INFO[code],
+                status: client.status,
+                config,
+                request: client
+            }
+            reject(res)
+        }
+
         const client = new XMLHttpRequest()
         // 所有请求都可以通过url传递数据；只有post、put和patch请求可以发送body
         if (params) {
@@ -69,21 +116,39 @@ function dispatchRequest(config){
             if(str.length > 0) str = str.substring(0, str.length - 1)
             url += url.indexOf('?')>-1 ? str : '?' + str
         }
+        // 始终会调用 loadend 无论成功还是失败
         // readystatechange会触发多次，优先使用loadend
-        if ('onloadend' in client) {
-            client.addEventListener('loadend', handler)
-        } else {
-            client.addEventListener('readystatechange', handler)
-        };
-		
-        client.open(config.method, url.substring(0,4) == 'http' ? url : config.baseURL + url, config.async)
-        client.responseType = config.responseType
-		
-        for (let name in headers) {
-            client.setRequestHeader(name, headers[name])
-        }
+        client.addEventListener('onloadend' in client ? 'loadend' : 'readystatechange', onLoadendHandler)
+        
+        // 下面是三个互斥事件 
+        // 超过 timeout 时间
+        client.addEventListener('timeout', onErrorHandler)
+        // 网络层错误
+        client.addEventListener('error', onErrorHandler)
+        // 调用 abort()
+        client.addEventListener('abort', onErrorHandler)
 
-        client.send(config.data ? JSON.stringify(config.data) : null)
+        try {
+            if (!url) {
+                throw new Error("INVALID-URL"); // 后面的代码不执行
+            }
+            client.timeout = Math.max(2000, config.timeout)
+            client.open(config.method, url.substring(0,4) == 'http' ? url : config.baseURL + url, config.async)
+            client.responseType = config.responseType
+            
+            for (let name in headers) {
+                client.setRequestHeader(name, headers[name])
+            }
+
+            client.send(config.data ? JSON.stringify(config.data) : null)
+
+        } catch (err) {
+            if(ERROR_INFO[err.message]) {
+                sendBeforeError(err.message)
+            }else{
+                sendBeforeError(err.name, err.message)
+            }
+        }
 
     })
     return promise
