6472c1dcee27ba03b64584302c3d07dec8892349
chocho <849890769@qq.com>
2026-05-21 19:13:01 +0800

    更新http模块

--- common/http/request.js
+++ common/http/request.js
@@ -1,5 +1,29 @@
+// AxiosError.ERR_BAD_OPTION_VALUE = 'ERR_BAD_OPTION_VALUE';
+// AxiosError.ERR_BAD_OPTION = 'ERR_BAD_OPTION';
+// AxiosError.ECONNABORTED = 'ECONNABORTED';
+// AxiosError.ETIMEDOUT = 'ETIMEDOUT';
+// AxiosError.ECONNREFUSED = 'ECONNREFUSED';
+// AxiosError.ERR_NETWORK = 'ERR_NETWORK';
+// AxiosError.ERR_FR_TOO_MANY_REDIRECTS = 'ERR_FR_TOO_MANY_REDIRECTS';
+// AxiosError.ERR_DEPRECATED = 'ERR_DEPRECATED';
+// AxiosError.ERR_BAD_RESPONSE = 'ERR_BAD_RESPONSE';
+// AxiosError.ERR_BAD_REQUEST = 'ERR_BAD_REQUEST';
+// AxiosError.ERR_CANCELED = 'ERR_CANCELED';
+// AxiosError.ERR_NOT_SUPPORT = 'ERR_NOT_SUPPORT';
+// AxiosError.ERR_INVALID_URL = 'ERR_INVALID_URL';
+// AxiosError.ERR_FORM_DATA_DEPTH_EXCEEDED = 'ERR_FORM_DATA_DEPTH_EXCEEDED';
+
+const ERROR_INFO = {
+    // 请求事件错误
+    "REQUEST-EVENT-TIMEOUT": "Request Timeout: The request exceeded the timeout limit",
+    "REQUEST-EVENT-ERROR": "Network Error: Failed to establish a connection",
+    "REQUEST-EVENT-ABORT": "Request Aborted: The request was manually cancelled",
+    // 请求前的代码验证错误/或程序错误
+    "INVALID-URL": "Invalid URL: Expected a non-empty string",
+}
+
 // 默认配置
-let defaultsConfig = {
+export const defaultsConfig = {
     data: null,
     params: null,
     baseURL: '',
@@ -14,17 +38,8 @@ let defaultsConfig = {
     }
 }
 
-const ERROR_INFO = {
-    // 请求事件错误
-    "REQUEST-EVENT-TIMEOUT": "Request Timeout: The request exceeded the timeout limit",
-    "REQUEST-EVENT-ERROR": "Network Error: Failed to establish a connection",
-    "REQUEST-EVENT-ABORT": "Request Aborted: The request was manually cancelled",
-    // 请求前的代码验证错误/或程序错误
-    "INVALID-URL": "Invalid URL: Expected a non-empty string",
-}
-
 // 配置合并
-function mergeConfig(config1, config2){
+export function mergeConfig(config1, config2){
     config1 = config1 || {}
     config2 = config2 || {}
     const headers = Object.assign({}, config1.headers, config2.headers)
@@ -33,7 +48,7 @@ function mergeConfig(config1, config2){
     return config
 }
 
-function dispatchRequest(config){
+export function dispatchRequest(config){
     // 合并默认配置和传入的配置
     config = mergeConfig(this.defaults, config)
     const headers = config.headers
@@ -154,106 +169,3 @@ function dispatchRequest(config){
     return promise
 }
 
-// 拦截器构造函数
-function InterceptorManager() {
-    this.handlers = []
-}
-InterceptorManager.prototype.use = function use(fulfilled, rejected, options) {
-    this.handlers.push({
-        fulfilled: fulfilled,
-        rejected: rejected,
-        synchronous: options ? options.synchronous : false,
-        runWhen: options ? options.runWhen : null
-    })
-    return this.handlers.length - 1
-}
-
-// 直接设置为null即可 无需剪切数组 这样每一项ID保持为项的数组索引不变，也避免了重新剪切拼接数组的性能损失。
-InterceptorManager.prototype.eject = function eject(id) {
-    if (this.handlers[id]) {
-        this.handlers[id] = null
-    }
-}
-
-function Http(config = {}){
-    // 强制使用new
-    if(!(this instanceof Http)){
-        return new Http(config)
-    }
-    // 配置
-    this.defaults = config
-    // 拦截器
-    this.interceptors = {
-        request: new InterceptorManager(),
-        response: new InterceptorManager()
-    }
-}
-Http.prototype.request = function(config = {}){
-    // 拦截器和请求组装队列
-    let chain = [dispatchRequest.bind(this), undefined] // 必须成对出现的
-
-    // 请求拦截
-    this.interceptors.request.handlers.forEach(interceptor => {
-        chain.unshift(interceptor.fulfilled, interceptor.rejected)
-    })
-
-    // 响应拦截
-    this.interceptors.response.handlers.forEach(interceptor => {
-        chain.push(interceptor.fulfilled, interceptor.rejected)
-    })
-
-    let promise = Promise.resolve(config)
-    while(chain.length > 0) {
-        promise = promise.then(chain.shift(), chain.shift())
-    }
-    return promise
-}
-// 添加请求方法
-Array.prototype.forEach.call([
-    'delete', 'get', 'head', 'options'
-], ele => {
-    Http.prototype[ele] = function(url, config) {
-        return this.request(Object.assign(config || {}, {
-            method: ele,
-            url,
-        }))
-    }
-})
-// 添加请求方法
-Array.prototype.forEach.call([
-    'post', 'put', 'patch'
-], ele => {
-    Http.prototype[ele] = function(url, data, config) {
-        return this.request(Object.assign(config || {}, {
-            method: ele,
-            url,
-            data
-        }))
-    }
-})
-
-// /////// 生成导出对象
-function createInstance(config) {
-    // 实例化一个对象
-    let context = new Http(config) // context.get（） context.post(), 但是不能当作函数使用 context() X
-    // 创建请求函数
-    let instance = Http.prototype.request.bind(context)// 等价 context.request(), 但是不能当作对象使用 属性和方法
-    // 把Http.prototype对象的方法添加到instance函数对象上
-    Object.keys(Http.prototype).forEach(key => {
-        instance[key] = Http.prototype[key].bind(context)
-    })
-    // 把Http实例属性添加到instance函数对象上
-    Object.keys(context).forEach(key => {
-        instance[key] = context[key]
-    })
-    return instance
-}
-
-let http = createInstance(defaultsConfig)
-
-// 构建新实例 这里只需要绑定在导出的http上 不需要绑定在用户创建的每个实例上
-http.create = function(config = {}){
-    return createInstance(mergeConfig(http.defaults, config)) // http.defaults 是全局的默认配置
-}
-
-export default http
