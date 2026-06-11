c74db5befde7a79e2fbe9f17d83445778bbcc679
chocho <849890769@qq.com>
2026-06-11 17:48:13 +0800

    Update isMiniProgram.js

--- core/utils/isMiniProgram.js
+++ core/utils/isMiniProgram.js
@@ -1,6 +1,6 @@
 function isMiniProgram() {
 	const ua = navigator.userAgent
-	// 微信小程序webview || 头条小程序webview
+	// 微信小程序webview（含桌面端） || 头条小程序webview
 	return (/miniProgram/i.test(ua) && /micromessenger/i.test(ua)) || /toutiaomicroapp/i.test(ua)
 }
 
