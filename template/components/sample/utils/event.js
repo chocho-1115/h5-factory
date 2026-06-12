78faee6ad1fa49413005980f6ccbcecb8ebfcc84
chocho <849890769@qq.com>
2026-06-12 16:32:01 +0800

    更新

---
+++ template/components/sample/utils/event.js
@@ -0,0 +1,31 @@
+
+import * as utils from '~/core/utils';
+
+
+const clickCall = {
+
+	// 复制userAgent
+	copyText() {
+		utils.copyText(navigator.userAgent, () => {
+			weui.toast('复制成功', 3000);
+		});
+	},
+	createFileControl(e) {
+		const accept = e.target.getAttribute('data-accept')
+		const fileEle = utils.createFileControl(accept, { successCallback: function (reader) { }, errorCallback: function (res) { } })
+		fileEle.click()
+	},
+
+};
+export const event = () => {
+
+	document.querySelector('.list').onclick = function (e) {
+		var ele = e.target;
+		if (ele.className !== 'btn') return;
+		var key = ele.getAttribute('data-key');
+		if (!key) return;
+		console.log(e)
+		clickCall[key] && clickCall[key](e);
+	}
+
+}
