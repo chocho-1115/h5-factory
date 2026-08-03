0125b12106b3632f0deca1dfb9d2db222842fa83
chocho <849890769@qq.com>
2026-08-03 15:33:20 +0800

    store

---
+++ template-react/store/clickStore.js
@@ -0,0 +1,10 @@
+// store/userStore.js
+import { create } from 'zustand';
+
+const useClickStore = create((set) => ({
+  click: 0,
+  add: () => set((state) => ({ click: state.click + 1 })),
+  clear: () => set({ click: 0 })
+}));
+
+export { useClickStore };
---
+++ template-react/store/userStore.js
@@ -0,0 +1,10 @@
+// store/userStore.js
+import { create } from 'zustand';
+
+const useUserStore = create((set) => ({
+  userInfo: null,
+  setUserInfo: (data) => set({ userInfo: data }),
+  clearUser: () => set({ userInfo: null })
+}));
+
+export { useUserStore };
